b9e10312db02f2f3d31f0283297390a8075b5dba
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-09 11:38:41 -0300

    Update Sem4At2Q4.py

--- Sem4At2Q4.py
+++ Sem4At2Q4.py
@@ -1,5 +1,2 @@
-a = int(input("Escreva um número, se ele for maior que 100 vai ser exibido True, se for menor que 100 será exibido False. "))
-if a >100:
-    print(True)
-else:
-    print(False)
+a = int(input("Insira um número para verificar se ele é maior que 100 (True) ou menor que 100 (False): "))
+print(a > 100)
